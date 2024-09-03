4678fa881496f361a6fe7442b688446d262d15de
prince-Arif30 <dadabolte30@gmail.com>
2024-09-04 02:06:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7254391805:AAHmv_DENnti5wn-UE9H7TiKvUIUx3o4yEE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("dadaxyt")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1057829913")
+
+
+
+
+
+
+
+  
